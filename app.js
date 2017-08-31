768d3948bbc4a3125148355dacc40fb6511b33ab
Peter Kincaid <loaatao@gmail.com>
2017-08-31 14:03:08 -0500

    This make so much more sense now

---
+++ app.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+  var color = "white";
+  $("#reset").on("click", function() {
+    $(".box").removeClass("white");
+  });
+  $("#red").on("click", function() {
+    color = "red";
+  });
+
+  $("#blue").on("click", function() {
+    color = "blue";
+  });
+
+  $("#green").on("click", function() {
+    color = "green";
+  });
+
+  $("#yellow").on("click", function() {
+    color = "yellow";
+  });
+
+  $("#white").on("click", function() {
+    color = "white";
+  });
+  $(".box").on("click", function() {
+    $(this).addClass(color);
+  });
+
+  $(".box").on("dblclick", function() {
+    $(this).removeClass(color);
+  });
+
+  $("#reset").on("click", function() {
+    $(".box").removeClass(color);
+  });
+});
